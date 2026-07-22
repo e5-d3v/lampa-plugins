1722eb7782385780223bfd6124f3883d7ad98cae
e5-d3v <80606142+e5-d3v@users.noreply.github.com>
2026-07-22 21:36:56 +0300

    Update ipstreamone.js

--- ipstreamone.js
+++ ipstreamone.js
@@ -1,8 +1,16 @@
 (function () {
     'use strict';
 
+    // Обязательный манифест для менеджера плагинов Лампы
+    Lampa.Manifest.plugins = {
+        type: 'other',
+        version: '1.0.0',
+        name: 'IPStream.one',
+        description: 'Тестовый плагин для проверки загрузки'
+    };
+
     if (window.Lampa && Lampa.Noty) {
-        Lampa.Noty.show('Плагин успешно загружен!');
+        Lampa.Noty.show('Плагин IPStream успешно загружен!');
     } else {
         console.log('IPStream plugin loaded');
     }
